df3d1b714be45ab4841cb16d2c3909d240c49c3e
Eugen Sadovnikov <sadeugen@gmail.com>
2023-04-16 20:59:17 +0300

    feat: implemantation count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  //let rezult=0;
+  //for(i=0;i<matrix.length;i++) rezult+=matrix[i].some(even => even==="^^");
+  //return rezult;
+  return matrix
+  .join(',')
+  .split(',')
+  .filter(item => item === "^^").length;
 }
 
 module.exports = {
