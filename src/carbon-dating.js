94c6c873a57a19e34cb4e512ae4215568cb6a589
Eugen Sadovnikov <sadeugen@gmail.com>
2023-04-17 00:11:31 +0300

    feat: implemantation carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample( sampleActivity ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!(typeof(sampleActivity)==="string")) return false;
+  let sampleActivityCount = Number(sampleActivity);
+  if (Boolean(sampleActivityCount)) {
+    if (!((sampleActivityCount>0) && (sampleActivityCount<=15))
+    ) return false;
+  } else return false;
+  
+  let k = 0.693/HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivityCount)/k);
 }
 
 module.exports = {
